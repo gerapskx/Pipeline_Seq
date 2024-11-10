efb1295e14044a5ec05ecbde6345dab0840b561d
Gerardo Flores-Iga <139929491+gerapskx@users.noreply.github.com>
2024-11-09 23:10:45 -0500

    Add files via upload

--- fastq_download.py
+++ fastq_download.py
@@ -5,20 +5,14 @@ from glob import glob
 import subprocess
 
 # initialization
-work_dir = './Cpepo_As'
+work_dir = './Drosophila_Pepper'
 samples = {
-    'Cpepo_100_1_S7_R1_001.fastq': 'SRR31111300',
-    'Cpepo_100_2_S8_R1_001.fastq': 'SRR31111299',
-'Cpepo_100_3_S9_R1_001.fastq': 'SRR31111298',
-'Cpepo_200_1_S10_R1_001.fastq': 'SRR31111297',
-'Cpepo_200_2_S11_R1_001.fastq': 'SRR31111296',
-'Cpepo_200_3_S12_R1_001.fastq': 'SRR31111295',
-'Cpepo_50_1_S4_R1_001.fastq': 'SRR31111305',
-    'Cpepo_50_2_S5_R1_001.fastq': 'SRR31111304',
-'Cpepo_50_3_S6_R1_001.fastq': 'SRR31111301',
-'Cpepo_CTRL_1_S1_R1_001.fastq': 'SRR31111294',
-'Cpepo_CTRL_2_S2_R1_001.fastq': 'SRR31111303',
-'Cpepo_CTRL_3_S3_R1_001.fastq': 'SRR31111302',
+    'TPR3': 'SRR20313491',
+    'TPR2': 'SRR20313492',
+'TPR1': 'SRR20313493',
+'CPR3': 'SRR20313494',
+'CPR3': 'SRR20313495',
+'CPR3': 'SRR20313496',
 }
 
 
